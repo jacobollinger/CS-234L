ff1ccfe8689a713054c4c6b3ea7e732cfde97676
BuildTools <unconfigured@null.spigotmc.org>
2020-09-01 21:37:39 -0500

    attempted half a solution

--- PS1/ReversableArrayQueue.java
+++ PS1/ReversableArrayQueue.java
@@ -2,28 +2,42 @@ class ReversibleArrayQueue<T> {
     private T[] arr;
     private int head;
     private int tail;
+    private boolean reversed;
 
     @SuppressWarnings("unchecked")
     public ReversibleArrayQueue(int capacity) {
         arr = (T[]) new Object[capacity + 1];
         head = 0;
         tail = 0;
+        reversed = false;
     }
 
     public void reverse() {
         //TODO
-        // int temp = head;
-        // head = tail;
-        // tail = temp;
+        reversed = reversed ? false : true;
+        int temp = head;
+        head = tail;
+        tail = temp;
     }
 
     public void enqueue(T value) {
         //TODO
-        // arr[tail] = value;
-        // tail = (tail + 1) % arr.length;
+        arr[tail] = value;
+        if (reversed) {
+            // tail = tail - 1;
+        } else {
+            tail = (tail + 1) % arr.length;
+        }
     }
 
     public T dequeue() {
-        return null;
+        //TODO
+        T value = arr[head];
+        if (reversed) {
+            // head = (head - 1);
+        } else {
+            head = (head + 1) % arr.length;
+        }
+        return value;
     }
 }
